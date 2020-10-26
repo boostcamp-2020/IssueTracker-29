538710d570110e1408b4d7acf6dad85cb4a77dc9
Yihyun <hihyun16@gmail.com>
2020-10-26 15:11:44 +0900

    Initialize eslint/prettier

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('view engine', 'html');
@@ -17,14 +17,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // routers
 
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
