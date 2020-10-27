c3d5963920c16b86a60ba06067b3960ba45d3b2e
Yihyun <hihyun16@gmail.com>
2020-10-27 19:40:21 +0900

    [FEAT] comment CRUD API 구현
    comment 관련 API 구현 및 routing 적용

---
+++ server/middlewares/comment.js
@@ -0,0 +1,41 @@
+const db = require('../models/connection');
+const { CREATE_COMMENT, READ_COMMENT, UPDATE_COMMENT, DELETE_COMMENT } = require('../models/query');
+
+const createComment = async (req, res) => {
+  const { contents, issueID } = req.body;
+  const userID = req.user.id;
+  if (!issueID) {
+    return res.status(403).json({ success: false, message: '없는 이슈입니다.' });
+  }
+  await db(CREATE_COMMENT, [contents, issueID, userID]);
+  return res.status(200).json({ success: true });
+};
+
+const readCommentsByIssueID = async (req, res) => {
+  const { issueid: issueID } = req.params;
+  const result = await db(READ_COMMENT, [issueID]);
+
+  return res.status(200).json({ success: true, result });
+};
+
+const updateComment = async (req, res) => {
+  const { contents, issueID } = req.body;
+  const { commentid: commentID } = req.params;
+  if (!issueID) {
+    return res.status(403).json({ success: false, message: '없는 이슈입니다.' });
+  }
+  await db(UPDATE_COMMENT, [contents, commentID]);
+  return res.status(200).json({ success: true });
+};
+
+const deleteComment = async (req, res) => {
+  const { issueID } = req.body;
+  const { coomentid: commentID } = req.params;
+  if (!issueID) {
+    return res.status(403).json({ success: false, message: '없는 이슈입니다.' });
+  }
+  await db(DELETE_COMMENT, [commentID]);
+  return res.status(200).json({ success: true });
+};
+
+module.exports = { createComment, readCommentsByIssueID, updateComment, deleteComment };
--- server/routes/issue.js
+++ server/routes/issue.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const commentMiddleware = require('../middlewares/comment');
+
 const router = express.Router();
+
+router.get('/:issueid/comment', commentMiddleware.readCommentsByIssueID);
 
 module.exports = router;
