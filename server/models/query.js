e646124a43e3ccae55ae02e42a64d8e562cd8185
hagyun <gimhayeong@gimhayeong-ui-MacBook-Air.local>
2020-10-28 15:03:03 +0900

    [FEAT] milestoneAPI
    DataBase Query 수정
    - 전체 milestone 목록 가져올 때, open된 milestone, close된 milestone 개수 같이 가져올수 있도록 수정
    - select 문에 서브쿼리 사용
    Insert문 구문 수정 - insert () into values (); -> insert into () values ();

--- server/models/query.js
+++ server/models/query.js
@@ -1,11 +1,12 @@
 module.exports = {
-  CREATE_COMMENT: 'insert comment(contents, issue_id, user_id) into values(?, ?, ?);',
+  CREATE_COMMENT: 'insert into comment(contents, issue_id, user_id) values(?, ?, ?);',
   READ_COMMENT: 'select id, contents from comment where issue_id = ?;',
   UPDATE_COMMENT: 'update comment set contents = ? where id = ?;',
   DELETE_COMMEN: 'delete from comment where id = ?;',
 
-  READ_MILESTONE: 'select id, title, due_date, description from milestone where is_open = ?;',
-  CREATE_MILESTONE: 'insert milestone(title, due_date, description, is_open, user_id) into values(?, ?, ?, ?, ?);',
+  READ_MILESTONE: `select id, title, due_date, description (select count(*) from milestone where is_open=1) as openCount, (select count(*) from milestone where is_open=0) as closeCount
+  from milestone where is_open = ?;`,
+  CREATE_MILESTONE: 'insert into milestone(title, due_date, description, is_open, user_id) values(?, ?, ?, ?, ?);',
   UPDATE_MILESTONE: 'update milestone set title = ?, due_date = ?, description = ? where id = ?;',
   DELETE_MILESTONE: 'delete from milestone where id = ?;',
 }
