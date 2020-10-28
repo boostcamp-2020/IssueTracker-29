3ab18f62ee817a56734330c723ed9e0f68edf57e
hagyun <gimhayeong@gimhayeong-ui-MacBook-Air.local>
2020-10-28 13:46:38 +0900

    [FEAT] milestone API
    Router 설정(GET, POST, PUT, DELETE)

--- server/routes/milestone.js
+++ server/routes/milestone.js
@@ -1,5 +1,10 @@
 const express = require('express');
-
+const milestoneMiddleware = require('../middlewares/milestone');
 const router = express.Router();
+
+router.get('/', milestoneMiddleware.readMilestone);
+router.post('/', milestoneMiddleware.createMilestone);
+router.put('/:milestoneid', milestoneMiddleware.updateMilestone);
+router.delete('/:milestoneid', milestoneMiddleware.deleteMilestone);
 
 module.exports = router;
