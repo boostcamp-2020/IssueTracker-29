365f9fb0572d357dd9c3e4ca1d0fab70338aff12
hagyun <gimhayeong@gimhayeong-ui-MacBook-Air.local>
2020-10-28 13:57:09 +0900

    [FEAT] milestone API
    Database CRUD Query문 작성

--- server/models/query.js
+++ server/models/query.js
@@ -3,4 +3,9 @@ module.exports = {
   READ_COMMENT: 'select id, contents from comment where issue_id = ?;',
   UPDATE_COMMENT: 'update comment set contents = ? where id = ?;',
   DELETE_COMMEN: 'delete from comment where id = ?;',
+
+  READ_MILESTONE: 'select id, title, due_date, description from milestone where is_open = ?;',
+  CREATE_MILESTONE: 'insert milestone(title, due_date, description, is_open, user_id) into values(?, ?, ?, ?, ?);',
+  UPDATE_MILESTONE: 'update milestone set title = ?, due_date = ?, description = ? where id = ?;',
+  DELETE_MILESTONE: 'delete from milestone where id = ?;',
 }
