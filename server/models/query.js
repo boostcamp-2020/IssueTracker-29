df6c9b61f7aaa79271e45ae8262f3b3a380c158a
Yihyun <hihyun16@gmail.com>
2020-10-28 18:33:55 +0900

    [FEAT] user 관련 query 추가
    user DB에 접근하기 위한 query 추가

--- server/models/query.js
+++ server/models/query.js
@@ -1,6 +1,8 @@
 module.exports = {
-  CREATE_COMMENT: 'insert comment(contents, issue_id, user_id) into values(?, ?, ?);',
+  CREATE_COMMENT: 'insert into comment(contents, issue_id, user_id) values(?, ?, ?);',
   READ_COMMENT: 'select id, contents from comment where issue_id = ?;',
   UPDATE_COMMENT: 'update comment set contents = ? where id = ?;',
-  DELETE_COMMEN: 'delete from comment where id = ?;',
+  DELETE_COMMENT: 'delete from comment where id = ?;',
+  CREATE_USER: 'insert into user(is_auth, username, password) values(?, ?, ?);',
+  READ_USER: 'select id, is_auth, username, password from user where username = ?;',
 }
