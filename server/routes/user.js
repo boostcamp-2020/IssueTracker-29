a4b208a4bdf0cb5ebd8efb20193d39e1c4309b2d
Yihyun <hihyun16@gmail.com>
2020-10-28 18:30:41 +0900

    [FEAT] user router 구현
    user API의 라우팅 처리

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const userMiddleWare = require('../middlewares/user');
 
 const router = express.Router();
+
+router.post('/signup', userMiddleWare.signup);
+
+router.post('/login', userMiddleWare.login);
+
+router.post('/logout', userMiddleWare.logout);
 
 module.exports = router;
