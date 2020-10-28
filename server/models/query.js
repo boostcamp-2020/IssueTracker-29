7bea14188c4a70b60fdc5ac3b1b1deace08ea823
bbbyung2 <bhko0524@naver.com>
2020-10-28 18:04:08 +0900

    [FEAT] label API
    label 관련 CRUD query문 작성
    DELETE_COMMEN => DELETE_COMMENT로 변경

--- server/models/query.js
+++ server/models/query.js
@@ -2,5 +2,10 @@ module.exports = {
   CREATE_COMMENT: 'insert comment(contents, issue_id, user_id) into values(?, ?, ?);',
   READ_COMMENT: 'select id, contents from comment where issue_id = ?;',
   UPDATE_COMMENT: 'update comment set contents = ? where id = ?;',
-  DELETE_COMMEN: 'delete from comment where id = ?;',
+  DELETE_COMMENT: 'delete from comment where id = ?;',
+
+  CREATE_LABEL: 'insert into label(name, description, color) values(?, ?, ?);',
+  READ_LABEL: 'select name, description, color from label;',
+  UPDATE_LABEL: 'update label set name = ?, description = ?, color = ? where id = ?;',
+  DELETE_LABEL: 'delete from label where id = ?;',
 }
