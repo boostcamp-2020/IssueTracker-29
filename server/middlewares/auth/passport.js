48ac88a62b826732cf3e9e890d3083dcdf353a72
Yihyun <hihyun16@gmail.com>
2020-10-29 18:32:34 +0900

    [FEAT] github 로그인 기능 구현
    passport와 jsonwebtoken을 이용한 github 로그인 후 token 반환 방식의 로그인 기능 구현

---
+++ server/middlewares/auth/passport.js
@@ -0,0 +1,26 @@
+const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
+const config = require('../../config')[process.env.NODE_ENV || 'development'];
+const { CREATE_USER, READ_USER } = require('../../models/query');
+const db = require('../../models/connection');
+
+module.exports = () => {
+  passport.serializeUser( (user, done) => {
+    return done(null, user);
+  });
+
+  const githubCallback = async (accessToken, refreshToken, profile, done) => {
+    let user = await db(READ_USER, [profile.username]);
+    if(!user) await db(CREATE_USER, [true, profile.username, null]);
+    user = await db(READ_USER, [profile.username]);
+    return done(null, user[0]);
+  };
+
+  passport.use(new GitHubStrategy({
+    clientID: config.githubOauthClientID,
+    clientSecret: config.githubOauthSecret,
+    callbackURL: config.githubOauthCallback,
+  },
+  githubCallback,
+  ));
+}
