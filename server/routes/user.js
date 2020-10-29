9a2ffb2db020dbcd46195f1ba5b3e244560ba376
Yihyun <hihyun16@gmail.com>
2020-10-29 18:33:59 +0900

    [FEAT] user router 구현
    github 로그인 및 토큰을 생성하는 router 구현

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const userMiddleWare = require('../middlewares/user');
+const passport = require('passport');
 
 const router = express.Router();
 
-router.post('/signup', userMiddleWare.signup);
-
-router.post('/login', userMiddleWare.login);
-
-router.post('/logout', userMiddleWare.logout);
+router.get('/auth/github/callback', passport.authenticate('github', {
+  failureRedirect: '/',
+}), userMiddleWare.createToken);
 
 module.exports = router;
