3718fed5b3411634c0ed4b3b311fdbde68cf7c44
bbbyung2 <bhko0524@naver.com>
2020-11-03 10:46:30 +0900

    [FEAT] issue 상세페이지 제목 및 내용 컴포넌트 설계
    issue 상세페이지를 구성하는 제목 관련 컴포넌트와 내용 관련 컴포넌트를 추가

---
+++ client/src/components/issueDetail/issueDetail.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { useIssues } from '../issue/api.js';
+import styled from 'styled-components';
+
+const IssueHeadContainer = styled.div`
+  border-bottom: 1px solid #d1d5da;
+  margin-bottom: 20px;
+`;
+
+const EditTitleButton = styled.button`
+  float: right;
+  padding: 10px;
+  margin-right: 100px;
+`;
+
+const SaveTitleButton = styled.button``;
+
+const CancelTitleButton = styled.button``;
+
+const EditContentsButton = styled.button``;
+
+const IssueBodyContainer = styled.div`
+  border: 1px solid #d1d5da;
+  width: 60%;
+`;
+
+const IssueContentsTopBar = styled.div`
+  background-color: #d1d5da;
+  border: 1px solid #d1d5da;
+`;
+
+const IssueContents = styled.div`
+  padding: 15px;
+`;
+
+const CloseIssueButton = styled.button``;
+const CommentIssueButton = styled.button``;
+
+const IssueDetail = ({ match }) => {
+  const [isOpen, setIsOpen] = useState(1);
+  const [title, setTitle] = useState();
+  const [contents, setContents] = useState();
+
+  const editTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const issues = useIssues();
+  const { id } = match.params;
+  const issue = issues[id - 1];
+
+  if (!issue) {
+    return <div>존재하지 않는 유저입니다.</div>
+  }
+  return (
+    <>
+      <IssueHeadContainer>
+        <EditTitleButton onClick={editTitle}>Edit</EditTitleButton>
+        <h3>{issue.title} #{issue.id}</h3>
+        {issue.is_open === 1 ? <p>Open</p> : <p>Closed</p>}
+      </IssueHeadContainer>
+      <IssueBodyContainer>
+        <IssueContentsTopBar>{issue.user_id} commented 3 days ago</IssueContentsTopBar>
+        <IssueContents>{issue.contents}</IssueContents>
+      </IssueBodyContainer>
+    </>
+  );
+};
+
+export default IssueDetail;
