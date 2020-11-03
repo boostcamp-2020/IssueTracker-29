c2017f73b2712dd7332cca651a8fc396df577acb
hagyun <gimhayeong@gimhayeong-ui-MacBook-Air.local>
2020-11-03 15:29:58 +0900

    [STYLE] New Issue - content 기본적인 Style 적용
    Content 작성 부분 스타일 적용

--- client/src/components/newIssue/contentContainer.js
+++ client/src/components/newIssue/contentContainer.js
@@ -8,7 +8,35 @@ const ContentContainer = styled.div`
 `;
 
 const TitleInput = styled.input``;
-const ContentTextarea = styled.textarea``;
+
+const ContentWrap = styled.div`
+    width: 300px;
+    height: auto;
+    position: relative;
+    display: inline-block;
+`;
+
+const TextCountSpan = styled.span`
+    position: absolute;
+    bottom: 2px;
+    right: 2px;
+`;
+
+const ContentTextarea = styled.textarea`
+    width: 100%;
+`;
+
+const ButtonContainer = styled.div`
+    display: flex;
+`;
+
+const CancelButton = styled.button`
+    background-color: white;
+    text-align: center;
+    text-decoration: none;
+    cursor: pointer;
+`;
+
 const SubmitButton = styled.button`
     background-color: #2EA44F;
     color: white;
@@ -34,11 +62,23 @@ const Content = (props) => {
         // alert(title + content);
     };
 
+    const cancelClickEvent = (e) => {
+
+    };
+    
     return (
         <ContentContainer>
             <TitleInput placeholder="Title" onChange={changeTitleData}/>
-            <ContentTextarea placeholder="Leave a comment" onChange={changeContentData}/>
-            <SubmitButton onClick={submitClickEvent}>Submit new issue</SubmitButton>
+            <div>Write</div>
+            <ContentWrap>
+                <ContentTextarea placeholder="Leave a comment" onChange={changeContentData} />
+                <TextCountSpan>characters</TextCountSpan>
+            </ContentWrap>
+            <div>Attach files by selecting here</div>
+            <ButtonContainer>
+                <CancelButton onClick={cancelClickEvent}>Cancel</CancelButton>
+                <SubmitButton onClick={submitClickEvent}>Submit new issue</SubmitButton>
+            </ButtonContainer>
         </ContentContainer>
     );
 };
