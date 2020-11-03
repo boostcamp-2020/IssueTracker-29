c1d618d9929dcc9a49346bad33dbf0cd8e60907d
Yihyun <hihyun16@gmail.com>
2020-11-03 10:41:23 +0900

    [REFACTOR] 미들웨어 구성 변경
    user 미들웨어를 의도에 맞게 사용하기 위해 미들웨어 구성 변경

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const tokenMiddleWare = require('../middlewares/auth/token');
 const userMiddleWare = require('../middlewares/user');
 const passport = require('passport');
 
@@ -6,6 +7,8 @@ const router = express.Router();
 
 router.get('/auth/github/callback', passport.authenticate('github', {
   failureRedirect: '/',
-}), userMiddleWare.createToken);
+}), tokenMiddleWare.createToken);
+
+router.get('/all', userMiddleWare.readAllUser);
 
 module.exports = router;
