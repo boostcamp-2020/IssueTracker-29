1b7738e9e3d127e66c4953d8b363e67e1e09b5bc
tejava <goddrinktejava@gmail.com>
2020-11-03 23:54:33 +0900

    [FEAT] 시간대로 나누어 보여주는 DatePassedViewer 구현
    시간대별로(분 등) 나누어 해당 시간에 해당하는 단위를 붙였다.
    또한 이를 시범적으로 issuelist에 적용하여 시간대를 표시하였다.

--- client/src/components/issuelist/issueItem.js
+++ client/src/components/issuelist/issueItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import DatePassedViewer from '../common/datePassed';
 import LabelItem from '../common/labelItem';
 import SvgCloseLogo from './svgCloseLogo';
 import SvgOpenLogo from './svgOpenLogo';
@@ -25,12 +26,22 @@ const LabelListContainer = styled.div`
 
 const IssueItem = (props) => {
   let isOpenLogo;
-
+  let issueInfoComponent;
   if (props.article.is_open) {
     isOpenLogo = <SvgOpenLogo color={COLOR_SUCCESS}/>
+    issueInfoComponent = (
+      <p>
+        #{props.article.id} opened <DatePassedViewer datetime={props.article.changed_at}/> ago by {props.article.username}
+      </p>
+    );
   }
   else {
     isOpenLogo = <SvgCloseLogo color={COLOR_DANGER}/>
+    issueInfoComponent = (
+      <p>
+        #{props.article.id} by {props.article.username} was closed <DatePassedViewer datetime={props.article.changed_at} />
+      </p>
+    );
   }
 
   const labelComponents = props.labels.map(item => <LabelItem key={item.id} label={item} />)
@@ -46,7 +57,7 @@ const IssueItem = (props) => {
             {labelComponents}
           </LabelListContainer>
         </TitleContainer>
-        <p>#{props.article.id} by {props.article.username}</p>
+        {issueInfoComponent}
       </div>
     </IssueItemContainer>
   )
