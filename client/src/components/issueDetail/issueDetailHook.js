0bdbc557c0865789b3bc92361df15717210c9e9c
bbbyung2 <bhko0524@naver.com>
2020-11-03 16:48:08 +0900

    [FIX] issue id로 issue를 조회하기 위한 hook 일부 설정
    함수명과 내부 로직 변경 예정

---
+++ client/src/components/issueDetail/issueDetailHook.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import { sendGetRequest } from '../common/api.js';
+
+const putIssuesInState = async (setIssues) => {
+    const issues = await sendGetRequest('/issue/:issueid');
+    setIssues(issues);
+}
+
+const useIssues = (id) => {
+  const [issues, setIssues] = useState([]);
+
+  useEffect(() => {
+    putIssuesInState(setIssues);
+  }, []);
+
+  return issues;
+}
+
+const putIssueLabelsInState = async (setLabels) => {
+  const labels = await sendGetRequest('/issue/label');
+  setLabels(labels);
+}
+
+const useIssueLabels = () => {
+  const [labels, setLabels] = useState([]);
+
+  useEffect(() => {
+    putIssueLabelsInState(setLabels);
+  }, []);
+
+  return labels;
+}
+
+
+
+export { useIssues, useIssueLabels };
