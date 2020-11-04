ae3d79687e4f4e063ca7b8de698a249d98ede97e
Yihyun <hihyun16@gmail.com>
2020-11-04 16:35:43 +0900

    [FEAT] 필터링 조건 문서화
    가독성을 위해 필터링 조건 문서화

---
+++ client/src/components/common/filterCondition.js
@@ -0,0 +1,9 @@
+const filterCondition = {
+  'Open issues': 'is:open ',
+  'Your issues': 'author:@me ',
+  'Everything assigned to you': 'assignee:@me ',
+  'Everything mentioning you': 'mentions:@me ',
+  'Closed issues': 'is:closed ',
+};
+
+export default filterCondition;
