07b6afa4f1dd54310fdbc69c6bc26aa86d957638
hagyun <gimhayeong@gimhayeong-ui-MacBook-Air.local>
2020-11-05 17:01:14 +0900

    [FEAT] issueDetail과 newIssue 공통으로 사용하는 부분 Common 폴더 및으로 이동
    issueSideBar, SvgSettingsLogo 부분 이동 및 적용

--- client/src/components/newIssue/newIssue.js
+++ client/src/components/newIssue/newIssue.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
 import styled from 'styled-components';
 import Content from './contentContainer';
+import IssueSideBar from '../common/issueSideBar';
+import { useIssueSideBarLabels } from './issueSideBarHook';
+import SvgSettingsLogo from '../common/svgSettingsLogo.js';
+
+const COLOR_SETTINGS = '#959da5';
 
 const NewIssueContainer = styled.div`
     margin-top: 100px;
@@ -9,11 +14,23 @@ const NewIssueContainer = styled.div`
 `;
 
 
-const NewIssue = (props) => {
+const NewIssue = ({match}) => {
     
+    const { id } = match.params;
+
+    const issueLabels = useIssueSideBarLabels(id);
+    const labels = [];
+    issueLabels.forEach(item => {
+        labels.push(item);
+    });
+
+    const svgSettingsIcon = <SvgSettingsLogo color={COLOR_SETTINGS}/>
+    const labelComponent = labels.map(item => <LabelItem key={item.id} label={item} />)
+
     return (
         <NewIssueContainer>
             <Content />
+            <IssueSideBar settingsIcon={svgSettingsIcon} labels={labelComponent} />
         </NewIssueContainer>
     )
 };
