37a82c74fb8b51c1dbb52638ed8c9f4d7dd64f79
Yihyun <hihyun16@gmail.com>
2020-11-05 18:27:17 +0900

    [FEAT] 로그인 링크 태그 추가
    Github OAuth를 통한 로그인 링크 추가
    아직 데이터베이스에 없는 사용자는 미작동

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -7,7 +7,12 @@ const Index = (props) => {
         props.history.push("/" + queryString);
     }
 
-    return (<Link to={`/issue?q=${encodeURIComponent(queryString)}`}><div>Hello</div></Link>);
+    return (
+      <>
+        <Link to={`/issue?q=${encodeURIComponent(queryString)}`}><div>Hello</div></Link>
+        <a href="https://github.com/login/oauth/authorize?client_id=c0c228aeba3f2b23bef5&redirect_uri=http://118.67.132.176/api/user/auth/github/callback">Login To Github</a>
+      </>
+    );
 }
 
 export default Index;
