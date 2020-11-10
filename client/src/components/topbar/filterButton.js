755c8ddc56d88ee9165de6361cffe474de36e37b
tejava <goddrinktejava@gmail.com>
2020-11-11 04:41:13 +0900

    [FEAT] topbar CSS 구현
    중복으로 사용되는 색은 common/color.js으로 올려 재사용성을 꾀했다.
    label, milestone을 위한 아이콘을 추가했다.
    topbar의 크기를 조절하였으며, flex로 검색창 길이를 조절하였다.
    또한, 내부의 들쭉날쭉한 component의 크기를 맞추었다.
    거의 모든 component를 styled component로 지정하였으며, border, color를 지정하였다.

--- client/src/components/topbar/filterButton.js
+++ client/src/components/topbar/filterButton.js
@@ -2,6 +2,27 @@ import React, {useState, useEffect, useContext} from 'react';
 import { Redirect } from 'react-router-dom';
 import Modal from '../common/modal';
 import filterCondition from '../common/filterCondition';
+import styled from 'styled-components';
+import { BORDER_COLOR, PRIMARY_COLOR } from '../common/color';
+
+const ButtonDiv = styled.div`
+  flex: 0 0 auto;
+`
+
+const ButtonInput = styled.input`
+  padding: 5px 16px;
+
+  background-color: #fafbfc;
+  color: ${PRIMARY_COLOR};
+  border: 1px solid ${BORDER_COLOR};
+
+  border-top-left-radius: 6px;
+  border-bottom-left-radius: 6px;
+
+  &:hover {
+    background-color: #f3f4f6;
+  }
+`;
 
 const FilterButton = (props) => {
   const [onModal, setOnModal] = useState(false);
@@ -21,11 +42,11 @@ const FilterButton = (props) => {
   };
 
   return (
-    <div>
-      <input type="button" value="Filters ▼" onClick={() => setOnModal(!onModal)} />
+    <ButtonDiv>
+      <ButtonInput type="button" value="Filters ▼" onClick={() => setOnModal(!onModal)} />
       <Modal onModal={onModal} title={title} items={option} onEvent={handleModalEvent} />
       {(!redirect)? null : <Redirect to={`/issue?=${encodeURIComponent(redirect).replace(/%20/g, '+')}`}/>}
-    </div>
+    </ButtonDiv>
   );
 };
 
