d8721f688ef663533668e9c237c29ccba52e9aaf
tejava <goddrinktejava@gmail.com>
2020-11-10 17:31:41 +0900

    [FEAT] FE가 유저 정보를 가져온 뒤 이를 context에 저장 구현
    또한, redirect를 적용하여 user가 존재하지 않는 경우 / 로 redirect하도록 하였다.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React, { useReducer, useState } from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { useEffect, useReducer, useState } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
 import styled, { createGlobalStyle } from 'styled-components';
 import Header from './components/header/header.js';
 import Login from "./components/login/index.js";
@@ -7,11 +7,13 @@ import Issue from "./components/issuelist/issuelist.js";
 import Label from './components/labellist/labellist.js';
 import NewIssue from "./components/newIssue/newIssue.js";
 import IssueDetail from "./components/issueDetail/issueDetail.js";
-import { IssueContext, LabelContext, MilestoneContext } from "./components/common/context.js";
+import { IssueContext, LabelContext, MilestoneContext, UserContext } from "./components/common/context.js";
 import asyncLabelWrapper from './wrapper/label';
 import { reducer as labelReducer} from './reducer/label';
 import MilestoneList from "./components/milestone/milestoneList.js";
 import NewMilestone from "./components/newMilestone/newMilestone.js";
+import { sendGetRequest } from "./components/common/api.js";
+import { BASE_API_URL } from "../util/config.js";
 
 const ResetStyle = createGlobalStyle`
   body {
@@ -32,30 +34,50 @@ const App = () => {
 
   const [labelState, labelDispatch] = useReducer(labelReducer, {labels: []});
   const asyncLabelDispatch = asyncLabelWrapper(labelDispatch);
-  
+
+  const [user, setUser] = useState(true);
+
+  const putUserInState = async () => {
+    const res = await sendGetRequest('/user/me', null);
+    console.log(res);
+    if (res !== null) {
+      return setUser(res);
+    }
+    setUser(null);
+  }
+
+  useEffect(() => {
+    putUserInState();
+  }, []);
+
+  const redirect = user ? null : <Redirect to="/"/>
+
   return (
     <div>
-        <ResetStyle />
-        <Header />
-          <ContentsContainer>
-            <IssueContext.Provider value={{issues, setIssues}}>
-              <LabelContext.Provider value={{labelState, labelDispatch: asyncLabelDispatch}}>
-                <MilestoneContext.Provider value={{milestones, setMilestones}}>
-                  <Switch>
-                    <Route exact path="/issue/create" component={NewIssue}/>
-                    <Route exact path="/issue/:id" component={IssueDetail} />
-                    <Route exact path="/issue" component={Issue}/>
-                  </Switch>
-                  <Route exact path='/label' component={Label}/>
-                  <Route exact path="/" component={Login}/>
-                  <Switch>
-                    <Route exact path="/milestone/create" component={NewMilestone}/>
-                    <Route exact path="/milestone" component={MilestoneList}/>
-                  </Switch>
-                </MilestoneContext.Provider>
-              </LabelContext.Provider>
-            </IssueContext.Provider>
-          </ContentsContainer>
+      <ResetStyle />
+      <Header />
+      <ContentsContainer>
+        <UserContext.Provider value={user}>
+          <IssueContext.Provider value={{issues, setIssues}}>
+            <LabelContext.Provider value={{labelState, labelDispatch: asyncLabelDispatch}}>
+              <MilestoneContext.Provider value={{milestones, setMilestones}}>
+                {redirect}
+                <Switch>
+                  <Route exact path="/issue/create" component={NewIssue}/>
+                  <Route exact path="/issue/:id" component={IssueDetail} />
+                  <Route exact path="/issue" component={Issue}/>
+                </Switch>
+                <Route exact path='/label' component={Label}/>
+                <Route exact path="/" component={Login}/>
+                <Switch>
+                  <Route exact path="/milestone/create" component={NewMilestone}/>
+                  <Route exact path="/milestone" component={MilestoneList}/>
+                </Switch>
+              </MilestoneContext.Provider>
+            </LabelContext.Provider>
+          </IssueContext.Provider>
+        </UserContext.Provider>
+      </ContentsContainer>
     </div>
   );
 }
