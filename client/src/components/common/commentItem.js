3c9656968acb4b81a9fd346fec9a9c27a66733f5
bbbyung2 <bhko0524@naver.com>
2020-11-10 10:58:32 +0900

    [FEAT] comment 변경 기능 구현
    [Edit] 버튼 클릭 시 contents를 textarea로 바꿔주어 변경 가능하게 함
    하지만 이것 역시 페이지를 직접 리로드하여야 변경된 사항이 반영됨 => 수정하기 위해서 코드를 조금 변경했지만 오류가 나는 상황..
    props로 전체 comment에 해당하는 setter인 setComments를 넘겨주어 filter하거나 reducer 사용으로 해결 예정

--- client/src/components/common/commentItem.js
+++ client/src/components/common/commentItem.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import ReactMarkdown from 'react-markdown';
 import DatePassedViewer from '../common/datePassed.js';
+import { sendPutRequest } from '../common/api';
 
 const CommentContainer = styled.div`
   border: 1px solid #d1d5da;
@@ -14,11 +15,45 @@ const CommentHeader = styled.div`
   border: 1px solid #d1d5da;
 `;
 
+const EditCommentButton = styled.button`
+  float: right;
+  padding: 10px;
+  margin-right: 100px;
+`;
+
 const CommentItem = (props) => {
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [content, setContent] = useState(props.comment.contents);
+
+  useEffect(() => {setContent(props.comment.contents)}, [props.comment.contents]);
+
+  const toggleIsEditing = () => {
+      setIsEditing(!isEditing);
+  };
+
+  const submitComment = async () => {
+      const res = await sendPutRequest(`/comment/${props.comment_id}`, {contents: content, issueID: props.issue.id});
+      if (res && res.success) {
+          props.setComments({...props.comment, contents: content});
+          setIsEditing(false);
+      }
+  };
+
+  const setContentState = (e) => {
+    setContent(e.target.value);
+  };
+
+  const cancelContentEdit = (e) => {
+      toggleIsEditing(e);
+      setContent(props.comment.contents);
+  }
+
   return (
     <CommentContainer>
         <CommentHeader>{props.comment.username} commented <DatePassedViewer datetime={props.comment.created_at} />  {props.issue_user_id == props.comment.user_id ? 'Owner' : null}</CommentHeader>
-        <ReactMarkdown source={props.comment.contents} />
+        {isEditing ? <div><button onClick={submitComment}>Update comment</button><button onClick={cancelContentEdit}>Cancel</button></div> : <EditCommentButton onClick={toggleIsEditing}>Edit</EditCommentButton>}
+        {isEditing ? <textarea value={props.comment.contents} onChange={setContentState}/> : <ReactMarkdown source={props.comment.contents} />}
     </CommentContainer>
   )
 }
