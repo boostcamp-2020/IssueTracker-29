029bb77218211e548068505081743f7e40e212ea
tejava <goddrinktejava@gmail.com>
2020-11-10 16:48:48 +0900

    [FEAT] 자기 자신의 정보를 돌려주는 router 구현

--- server/middlewares/user.js
+++ server/middlewares/user.js
@@ -6,4 +6,13 @@ const readAllUser = async (req, res) => {
   return res.status(200).json({ result });
 };
 
-module.exports = { readAllUser };
+const readIsLogIn = async (req, res, next) => {
+  if (req.user) {
+    const {id, username, profile} = req.user;
+    return res.status(200).json({success: true, result: {id, username, profile}});
+  }
+
+  return res.status(401).json({success: false});
+};
+
+module.exports = { readAllUser, readIsLogIn };
