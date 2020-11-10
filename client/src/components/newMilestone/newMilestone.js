7d0fd6c6204bc5f238f73a51adc08bde0d0e6355
Yihyun <hihyun16@gmail.com>
2020-11-10 17:36:36 +0900

    [FEAT] 새 마일스톤 생성 기능 구현
    마일스톤 생성 페이지의 유효성 검사 및 생성 기능 동작하게 함

--- client/src/components/newMilestone/newMilestone.js
+++ client/src/components/newMilestone/newMilestone.js
@@ -1,32 +1,64 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import styled from 'styled-components';
+import { sendPostRequest } from '../common/api';
+import Content from '../newIssue/contentContainer';
+
+const DateInput = styled.input`
+  color: ${props => props.color};
+`;
 
 const NewMilestone = (props) => {
   const [title, setTitle] = useState("");
-  const [date, setDate] = useState(new Date());
+  const [dueDate, setDueDate] = useState(new Date());
+  const [dateColor, setDateColor] = useState('black');
   const [description, setDescription] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  useEffect(() => {
+    verifyDate(dueDate);
+  }, [dueDate])
+
+  const verifyDate = (dueDate) => {
+    // TODO: due date가 유효한지 (현재 날짜보다 이후의 날짜인지) 체크 -> 유효하지 않으면 빨간색으로 표시
+    const current = new Date();
+    if (current.getFullYear() > dueDate.getFullYear() || current.getMonth() > dueDate.getMonth() || current.getDate() > dueDate.getDate())
+      setDateColor('red');
+    else
+      setDateColor('black');
+  };
+
+  const createMilestone = async () => {
+    // TODO: Title Null 체크, 마일스톤 생성 후 마일스톤 목록 페이지로 리다이렉트
+    if (!title.length) return alert('제목을 입력해주세요.');
+    if (dateColor === 'red') return alert('유효한 날짜를 입력해주세요.');
+    await sendPostRequest('/milestone', {title, dueDate: dueDate.toISOString().slice(0, 10), description});
+    setRedirect(true);
+  };
 
   return (
     <div>
       <div>
-
+        <h2>New Milestone</h2>
+        Create a new milestone to help organize your issues and pull requests. Learn more about milestones and issues.
+      </div>
+      <hr />
+      <div>
+        <h3>title</h3>
+        <input type="text" onChange={e => setTitle(e.target.value)} />
       </div>
       <div>
-        <label>
-          Title
-          <input/>
-        </label>
-        <label>
-          Due date (optional)
-          <input type="date" onChange={(e) => console.log(new Date(e.target.value))}/>
-        </label>
-        <label>
-          Description (optional)
-          <textarea value={description} onChange={e => setDescription(e.target.value)}/>
-        </label>
+        <h3>Due date (optional)</h3>
+        <DateInput type="date" color={dateColor} onChange={(e) => setDueDate(new Date(e.target.value))} />
       </div>
       <div>
-        <button>Create milestone</button>
+        <h3>Description (optional)</h3>
+        <textarea value={description} onChange={e => setDescription(e.target.value)} rows="15" cols="100" />
       </div>
+      <div>
+        <button onClick={createMilestone}>Create milestone</button>
+      </div>
+      {redirect? <Redirect to='/milestone' /> : null}
     </div>
   )
 }
