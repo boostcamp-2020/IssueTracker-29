b4ba83729998bd84a81b7d51c6e011aefe2deabb
bbbyung2 <bhko0524@naver.com>
2020-11-11 17:37:59 +0900

    [FEAT] issue sidebar에 assignees, labels, milestone 모달 창 추가
    assignees의 경우 user의 username, profile 출력
    labels의 경우 label의 color, name, description 출력
    milestone의 경우 milestone의 title 출력

---
+++ client/src/components/common/sideBarItem.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SideBarContainer = styled.div`
+    width: 80%;
+    // height: 50px;
+    border: 1px solid black;
+`;
+
+const ColorBox = styled.div`
+    width: 20px;
+    height: 20px;
+    background-color: ${props => props.color};
+    border-radius: 5px;
+`;
+
+const ProfileBox = styled.img`
+    width: 20px;
+    height: 20px;
+    src: ${props => props.src};
+    border: 1px solid black;
+`;
+
+const sideBarItem = (props) => {
+    console.log(props)
+  return (
+    <SideBarContainer>
+        {props.username ?
+            <>
+                <ProfileBox src={props.profile} />
+                {/* <div>{props.profile}</div> */}
+                <div>{props.username}</div>
+            </>
+        : null}
+        {props.name ? 
+            <>
+                <ColorBox color={props.color} />
+                <div>{props.name}</div>
+                <div>{props.description}</div>
+            </>
+        : null}
+        {props.title ? <p>{props.title}</p> : null}
+    </SideBarContainer>
+  )
+}
+
+export default sideBarItem;
