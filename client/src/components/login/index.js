5113c610eb6be920becf8b30b6ce46f24dc7b81a
Yihyun <hihyun16@gmail.com>
2020-11-12 15:09:54 +0900

    [FEAT] Github OAuth를 통한 로그인 버튼에 Github Icon 추가
    svg를 통한 Github Icon 추가

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { BASE_API_URL } from '../../../util/config';
+import GithubLogo from '../common/icon/svgGithubIcon';
 
 const GithubLink = styled.a`
-  width: 20%;
-  display: block;
+  display: flex;
+  justify-content: space-around;
+  width: 15%;
   margin: 0 auto;
   padding: .5rem;
   border-radius: 8px;
   color: white;
-  text-align: center;
   text-decoration: none;
   background-color: black;
   cursor: pointer;
@@ -20,7 +21,10 @@ const Index = (props) => {
     return (
       <>
         <Link to={`/issue`}><div>바로 이슈로 (테스트용)</div></Link>
-        <GithubLink href={BASE_API_URL + '/user/auth/github'}>Sign in with Github</GithubLink>
+        <GithubLink href={BASE_API_URL + '/user/auth/github'}>
+          <span>Sign in with Github</span>
+          <GithubLogo color="black" />
+        </GithubLink>
       </>
     );
 }
